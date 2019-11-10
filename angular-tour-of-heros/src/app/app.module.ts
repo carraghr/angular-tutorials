c960be771887b7ad5e65b0b03164c613b9762e84
carraghr <carraghr@gmail.com>
2019-11-10 17:32:04 +0000

    tutorial completed up to routing section

--- angular-tour-of-heros/src/app/app.module.ts
+++ angular-tour-of-heros/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
