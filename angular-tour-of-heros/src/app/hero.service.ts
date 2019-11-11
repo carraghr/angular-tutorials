91bcbd6383aee5befe03487a86cff8f4132a1b0b
Richard Carragher <carraghr@users.noreply.github.com>
2019-11-11 23:36:27 +0000

    complete upload of routing done

--- angular-tour-of-heros/src/app/hero.service.ts
+++ angular-tour-of-heros/src/app/hero.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 
+import {Observable, of} from "rxjs";
+
 import {Hero} from './hero'
 import {HEROES} from "./mock-heroes";
-import {Observable, of} from "rxjs";
 import {MessageService} from "./message.service";
 
 @Injectable({
@@ -12,9 +13,15 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]>{
-    //TODO: send the message _after_ fetching the heroes
-    this.messageService.add("HeroService: fetched heroes");
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
